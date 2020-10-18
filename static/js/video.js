1bc5a575918c3ddd85f20ea78e100dea558a4a71
Nick Price <nick@spun.io>
2020-10-18 01:34:03 +0000

    new

--- static/js/video.js
+++ static/js/video.js
@@ -1,5 +1,7 @@
 /// <reference path="./both.js" />
 function initPlayer() {
+    clearTimeout(window.playerReload);
+
     if (!flvjs.isSupported()) {
         console.warn('flvjs not supported');
         return;
@@ -20,6 +22,41 @@ function initPlayer() {
     };
 
     let flvPlayer = flvjs.createPlayer(mediaDataSource, playerConfig);
+
+    flvPlayer.on(flvjs.Events.ERROR, function(a,b,c) {
+        //network or other error (probably a 404 from no video)
+        //HttpStatusCodeInvalid
+        //MediaMSEError will blow it up
+	console.log(b);
+        if (b == "HttpStatusCodeInvalid") {
+            console.log("Setting reload timeout.");
+            document.getElementById("noVideo").style.display="flex";
+            this.playerReload = setTimeout(function(){
+                this.unload();
+                this.load();
+                this.play();
+            }.bind(flvPlayer), 3000);
+        }
+    });
+
+    flvPlayer.on(flvjs.Events.LOADING_COMPLETE, function(a,b,c) {
+        //the stream is over
+        console.log("Setting reload timeout.");
+        document.getElementById("noVideo").style.display="flex";
+        this.playerReload = setTimeout(function(){
+            this.unload();
+            this.load();
+            this.play();
+        }.bind(flvPlayer), 3000);
+    });
+
+    flvPlayer.on(flvjs.Events.STATISTICS_INFO, function(a,b,c) {
+        //we are playing video so hide this thing
+        clearTimeout(this.playerReload);
+        document.getElementById("noVideo").style.display="none";
+    }.bind(flvPlayer));
+
+
 
     flvPlayer.attachMediaElement(videoElement);
     flvPlayer.load();
